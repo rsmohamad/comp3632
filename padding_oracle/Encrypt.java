792cc0df7e3c9592a25bdf3c31bd1ea66e797602
Randitya <rsmohamad@users.noreply.github.com>
2017-11-13 14:15:55 +0800

    Add comments to Encrypt.java

--- padding_oracle/Encrypt.java
+++ padding_oracle/Encrypt.java
@@ -25,16 +25,23 @@ public class Encrypt {
 
     public void encrypt() throws IOException {
         ArrayList<byte[]> encrypted = new ArrayList<>();
+        
+        // Start with a random block
+        // Last block = random block
         byte y[] = new byte[16];
         new Random().nextBytes(y);
         encrypted.add(y);
 
+        // For every block in the plaintext:
+        // C_i-1 = Dec(C_i) xor P_i
+        // Work from the last block until the IV
         for (int i = blocks.size() - 1; i >= 0; i--) {
             y = Decrypt.decryptBlock(y);
             y = Decrypt.xorBlocks(y, blocks.get(i));
             encrypted.add(y);
         }
-
+        
+        // Print [IV|C_0|...|C_n-1] to stdout
         for (int i = encrypted.size() - 1; i >= 0; i--)
             System.out.write(encrypted.get(i));
     }
